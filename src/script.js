801a151c204eb7a2f2a75411726cab6e28546fae
kevin <kevnhech@gmail.com>
2024-11-15 11:16:06 -0800

    Add a toggle input

--- src/script.js
+++ src/script.js
@@ -10,11 +10,11 @@ async function getWeather(location) {
     description: weatherData.currentConditions.conditions,
   };
 
-  const body = document.querySelector("body");
+  // Display weather information on page
 
+  const body = document.querySelector("body");
   const weatherInfo = document.createElement("div");
   weatherInfo.setAttribute("class", "weather-info");
-
   const address = document.createElement("p");
   const temperature = document.createElement("p");
   const description = document.createElement("p");
@@ -28,6 +28,23 @@ async function getWeather(location) {
   weatherInfo.appendChild(description);
 
   body.appendChild(weatherInfo);
+  addToggle();
+}
+
+function addToggle() {
+  const body = document.querySelector("body");
+
+  const label = document.createElement("label");
+  label.setAttribute("class", "switch");
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  const slider = document.createElement("span");
+  slider.setAttribute("class", "slider round");
+
+  label.appendChild(checkbox);
+  label.appendChild(slider);
+
+  body.appendChild(label);
 }
 
 getWeather("Las Vegas");
@@ -36,10 +53,14 @@ const cityInput = document.querySelector("#city");
 const button = document.querySelector("button");
 
 button.addEventListener("click", function (e) {
-  // remove the div from the body first, then append a new div
   e.preventDefault();
   const weatherInfo = document.querySelector(".weather-info");
+  const label = document.querySelector(".switch");
   weatherInfo.remove();
+  label.remove();
   getWeather(cityInput.value);
   cityInput.value = "";
 });
+
+// fahrenheit = "unitGroup=us&"
+// celsius = "unitGroup=uk&"
