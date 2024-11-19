d414aff273fd0617463fbb5857284879a22c32a6
kevin <kevnhech@gmail.com>
2024-11-18 20:23:58 -0800

    Write a function that changes background image based on weather

--- src/script.js
+++ src/script.js
@@ -1,4 +1,13 @@
 import "./styles.css";
+import clearDay from "./images/clear-day.jpg";
+import clearNight from "./images/clear-night.jpg";
+import cloudy from "./images/cloudy.jpg";
+import fog from "./images/fog.jpg";
+import partlyCloudyDay from "./images/partly-cloudy-day.jpg";
+import partlyCloudyNight from "./images/partly-cloudy-night.jpg";
+import rain from "./images/rain.jpg";
+import snow from "./images/snow.jpg";
+import wind from "./images/wind.jpg";
 
 async function getWeather(location) {
   const toggle = document.querySelector(".toggle");
@@ -12,8 +21,31 @@ async function getWeather(location) {
   
   const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?${unit}&key=M946UUHWEQHTTPVLK3GCKTAQ8`, {mode: "cors"});
   const weatherData = await response.json();
-  console.log(weatherData);
 
+  function setBackgroundImage() {
+    const image = document.createElement("img");
+    if (weatherData.currentConditions.icon == "clear-day") {
+      image.src = clearDay;
+    } else if (weatherData.currentConditions.icon == "clear-night") {
+      image.src = clearNight;
+    } else if (weatherData.currentConditions.icon == "cloudy") {
+      image.src = cloudy;
+    } else if (weatherData.currentConditions.icon == "fog") {
+      image.src = fog;
+    } else if (weatherData.currentConditions.icon == "partly-cloudy-day") {
+      image.src = partlyCloudyDay;
+    } else if (weatherData.currentConditions.icon == "partly-cloudy-night") {
+      image.src = partlyCloudyNight;
+    } else if (weatherData.currentConditions.icon == "rain") {
+      image.src = rain;
+    } else if (weatherData.currentConditions.icon = "snow") {
+      image.src = snow;
+    } else if (weatherData.currentConditions.icon == "wind") {
+      image.src = wind;
+    }
+    body.style.backgroundImage = `url(${image.src})`;
+  }
+  console.log(weatherData.currentConditions.icon);
   const obj = {
     address: weatherData.resolvedAddress.split(",")[0],
     temperature: weatherData.currentConditions.feelslike,
@@ -44,6 +76,7 @@ async function getWeather(location) {
   weatherInfo.appendChild(description);
 
   body.appendChild(weatherInfo);
+  setBackgroundImage();
 }
 
 getWeather("Las Vegas");
